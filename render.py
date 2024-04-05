1f4b1757331e353f7c91d0e411b38a83e19416bd
Diego Kiedanski <dkiedanski@gmail.com>
2024-04-05 09:50:47 -0700

    adding faq

--- render.py
+++ render.py
@@ -1,7 +1,7 @@
 import shutil
 import datetime
 import re
-import markdown
+# import markdown
 import bleach
 import urllib.request
 import os
@@ -9,11 +9,18 @@ import csv
 import json
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from urllib.parse import urlparse, parse_qs
+import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import html
 
 
 SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRRJBT6BewhEneccXKdgp2EK6SXDFxv4dJ8XRDbAVrUAV1B2xGiEiI3okoSwvkU09pNwx8oc8MvKWkv/pub?gid=696886356&single=true&output=csv'
+FAQ_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRRJBT6BewhEneccXKdgp2EK6SXDFxv4dJ8XRDbAVrUAV1B2xGiEiI3okoSwvkU09pNwx8oc8MvKWkv/pub?gid=2108387313&single=true&output=csv"
 
-SAVE_PATH = 'data.json'
+SAVE_PATH_DATA = 'data.json'
+SAVE_PATH_FAQ = "faq.json"
+
 
 # shutil.rmtree("static/", ignore_errors=True)
 os.makedirs("static", exist_ok=True)
@@ -22,8 +29,42 @@ os.makedirs("static/img", exist_ok=True)
 shutil.copy("libs/htmx.org@1.9.11", "static/")
 shutil.copy("libs/tailwind.min.css", "static/")
 
+class HighlightRenderer(mistune.HTMLRenderer):
+    def block_code(self, code, info=None):
+        if info:
+            lexer = get_lexer_by_name(info, stripall=True)
+            formatter = html.HtmlFormatter()
+            return highlight(code, lexer, formatter)
+        return '<pre><code>' + mistune.escape(code) + '</code></pre>'
+
 
 last_updated = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M UTC")
+
+import re
+
+def add_pre_tags_around_code_regex(html_str):
+    """
+    Wraps <pre> tags around <code> tags in the given HTML string using regex.
+
+    Args:
+    - html_str (str): The input HTML string.
+
+    Returns:
+    - str: The modified HTML string with <pre> tags wrapped around <code> tags.
+    """
+    # Define a regex pattern to match <code>...</code> blocks
+    pattern = r'(<code>.*?</code>)'
+    
+    # Define a function to be applied to each match
+    def wrap_with_pre(match):
+        return '<pre>' + match.group(1) + '</pre>'
+    
+    # Use re.sub() to apply the wrap_with_pre function to each match
+    modified_html = re.sub(pattern, wrap_with_pre, html_str, flags=re.DOTALL)
+    
+    return modified_html
+
+
 
 
 def make_url_safe_remove_unsafe(s):
@@ -34,10 +75,10 @@ def make_url_safe_remove_unsafe(s):
 
 # Test the function with a sample string
 
-def download_sheet_as_json():
+def download_sheet_as_json(URL, SAVE_PATH):
 
     try:
-        with urllib.request.urlopen(SHEET_URL) as response, open(SAVE_PATH, 'w') as out_file:
+        with urllib.request.urlopen(URL) as response, open(SAVE_PATH, 'w') as out_file:
             data = response.read().decode("utf-8")  # Read the data from the URL
             print(data)
             data = [dict(r) for r in csv.DictReader(data.split("\n"))]
@@ -79,12 +120,16 @@ def card_to_html(card_json):
     card_json["has_image"] = False
 
 
+    markdown_render = mistune.create_markdown(renderer=HighlightRenderer())
 
     has_image = False
     for key in ["front", "prompt", "answer", "analysis"]:
         text = card_json[key]
-        html = markdown.markdown(text).replace("\\n", "<br>")
+        # html = markdown.markdown(text)
+        html = markdown_render(text).replace("\\n", "<br>")
+
         safe_answer = bleach.clean(html, tags=['p', 'strong', 'em', 'ul', 'li', 'h1', 'h2', 'h3', 'pre', 'code', 'br'], strip=True)
+        safe_answer = add_pre_tags_around_code_regex(safe_answer)
 
         if "@img1" in text:
             has_image = True
@@ -106,12 +151,18 @@ def card_to_html(card_json):
 
 data = []
 download_images = False
-if os.path.exists(SAVE_PATH):
+if os.path.exists(SAVE_PATH_DATA):
     print("Skipping download")
-    with open(SAVE_PATH, "r") as fh: data = json.load(fh)
+    with open(SAVE_PATH_DATA, "r") as fh: data = json.load(fh)
 else:
-    data = download_sheet_as_json()
+    data = download_sheet_as_json(SHEET_URL, SAVE_PATH_DATA)
     download_images = True
+
+if os.path.exists(SAVE_PATH_FAQ):
+    print("Skipping download")
+    with open(SAVE_PATH_FAQ, "r") as fh: faq = json.load(fh)
+else:
+    faq = download_sheet_as_json(FAQ_URL, SAVE_PATH_FAQ)
 
 data = list(filter(lambda x: x["id"] != "", data))
 
@@ -148,6 +199,9 @@ with open('static/index.html', 'w') as f:
 
 print("Site generated successfully.")
 
+print(faq)
+faq_html = env.get_template("faq.html").render(faq_data=faq, last_updated=last_updated)
+with open("static/faq.html", "w") as f: f.write(faq_html)
 
 
 
